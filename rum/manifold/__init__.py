dcc7df646ac1d51657c3b248157301977370b8dc
Andrea Pinto <andreakiro.pinto@gmail.com>
2024-02-07 10:47:58 -0500

    bugfix to use rum package

--- rum/manifold/__init__.py
+++ rum/manifold/__init__.py
@@ -1,6 +1,6 @@
 from rum.manifold.manifold import Manifold, GeodesicManifold
 from rum.manifold.euclidean import EuclideanManifold
-from rum.manifold.sphere import SphereManifold
-from rum.manifold.torus import TorusManifold
-from rum.manifold.hyperboloid import HyperboloidManifold
-from rum.manifold.hyperbolic_parabola import HyperbolicParabolaManifold
+# from rum.manifold.sphere import SphereManifold
+# from rum.manifold.torus import TorusManifold
+# from rum.manifold.hyperboloid import HyperboloidManifold
+# from rum.manifold.hyperbolic_parabola import HyperbolicParabolaManifold
